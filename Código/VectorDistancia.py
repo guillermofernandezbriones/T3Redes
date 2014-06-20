9ac995e5be92277f5b46bdd8438bd4ade215e9fb
Álvaro <alvaro.rojasv@alumnos.usm.cl>
2014-06-19 20:12:04 -0400

    final tablas de ruteo + informe segunda parte
    Se eliminó la parte de código
    Se empezó a trabajar en la segunda parte del informe
    Se terminó de trabajar las tablas de ruteo a mano:
    
    https://docs.google.com/a/alumnos.usm.cl/spreadsheets/d/1twOYTx-pRIBnfFSWtBbT51HUh8tqld4kWFh-agUpb74/edit#gid=0

--- Código/VectorDistancia.py
+++
@@ -1,9 +0,0 @@
-# Matrices de adyasencia+costo
-import struct
-#                      A                    B                   C                   D                   E                   F                   G                   H                   I
-#              A B C D E F G H I    A B C D E F G H I   A B C D E F G H I   A B C D E F G H I   A B C D E F G H I   A B C D E F G H I   A B C D E F G H I   A B C D E F G H I    A B C D E F G H I
-matriz= array([0,1,0,0,0,0,4,0,10],[1,0,9,0,8,0,0,0,0],[0,9,0,2,0,0,0,0,0],[0,0,2,0,9,4,0,0,2],[0,8,0,9,0,2,0,0,1],[0,0,0,4,2,0,0,6,0],[4,0,0,0,0,0,0,7,0],[0,0,0,0,0,6,7,0,3],[10,0,0,2,1,0,0,3,0])
-
-print matriz
-
-
